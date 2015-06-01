f9bf91fcf3136e5c6666ff11e43354b55c958a41
luiz <luiz@luiz-System-Product-Name>
2015-06-01 13:42:19 -0300

    Initial commit

---
+++ crawler/src/main/java/com/sillyapps/severino/crawler/App.java
@@ -0,0 +1,13 @@
+package com.sillyapps.severino.crawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ crawler/src/main/java/com/sillyapps/severino/crawler/parsers/Advogados.java
@@ -0,0 +1,5 @@
+package com.sillyapps.severino.crawler.parsers;
+
+public class Advogados {
+
+}
